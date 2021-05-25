8522ca900bb96fe57a0b67d45d8b635ca7591ada
Nocino <zanuccod@gmail.com>
2021-05-25 10:44:03 +0200

    StorageQueue: Add ReadMessages
    Fix commit

--- QueueStorage/Program.cs
+++ QueueStorage/Program.cs
@@ -7,8 +7,8 @@ namespace QueueStorage
 {
     class Program
     {
-        private static readonly string connectionStr = "DefaultEndpointsProtocol=https;AccountName=storagericopegnata;AccountKey=HcT0CARsLLV5eKHQJju/V1+4W4NPw/znWQLqzJLs/dN6PfOZjJbqCHkWqaBW5dvJ6Hg/wlmlJdu3Exe9sfxzZA==;EndpointSuffix=core.windows.net";
-        private static readonly string queueName = "ricopegnataqueue";
+        private static readonly string connectionStr = "";
+        private static readonly string queueName = "";
 
         static void Main(string[] args)
         {
@@ -21,7 +21,8 @@ namespace QueueStorage
                 Console.WriteLine("Insert a valid input");
             }
 
-            switch(input.ElementAt(0))
+            int nOfMessages = 0;
+            switch (input.ElementAt(0))
             {
                 case 'n':
                     AddMessages(input[2..])
@@ -30,9 +31,22 @@ namespace QueueStorage
                     break;
 
                 case 'p':
-                    if (int.TryParse(input[2..], out var nOfMessages))
+                    if (int.TryParse(input[2..], out nOfMessages))
                     {
                         PeakMessages(nOfMessages)
+                            .GetAwaiter()
+                            .GetResult();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Given input <{input[2..]}> is not a valid number");
+                    }
+                    break;
+
+                case 'r':
+                    if (int.TryParse(input[2..], out nOfMessages))
+                    {
+                        ReadMessages(nOfMessages)
                             .GetAwaiter()
                             .GetResult();
                     }
@@ -50,20 +64,22 @@ namespace QueueStorage
 
         private static async Task AddMessages(string message)
         {
-            var queueClient = new QueueClient(connectionStr, queueName);
-
-            await queueClient.CreateIfNotExistsAsync();
-
+            var queueClient = await GetQueueClient();
             await queueClient.SendMessageAsync(message);
 
             Console.WriteLine($"New message added to the queue <{queueName}>, message: <{message}>");
         }
 
+        // Peak, Read, Delete, Receive
+
+        /// <summary>
+        /// Read but leave on the queue the messages
+        /// </summary>
+        /// <param name="numberOfMessages"></param>
+        /// <returns></returns>
         private static async Task PeakMessages(int numberOfMessages)
         {
-            var queueClient = new QueueClient(connectionStr, queueName);
-
-            await queueClient.CreateIfNotExistsAsync();
+            var queueClient = await GetQueueClient();
 
             var items = await queueClient.PeekMessagesAsync(numberOfMessages);
 
@@ -73,11 +89,38 @@ namespace QueueStorage
             }
         }
 
+        /// <summary>
+        /// Get and remove from queue the messages
+        /// </summary>
+        /// <param name="numberOfMessages"></param>
+        /// <returns></returns>
+        private static async Task ReadMessages(int numberOfMessages)
+        {
+            var queueClient = await GetQueueClient();
+
+            var items = await queueClient.ReceiveMessagesAsync(numberOfMessages);
+
+            for (var i = 0; i < items.Value.Length; i++)
+            {
+                Console.WriteLine($"{i}: <{items.Value.ElementAt(i).MessageId}> <{items.Value.ElementAt(i).Body}>");
+            }
+        }
+
         private static void PrintIntroMessages()
         {
             Console.WriteLine("Type one of the following commands:");
             Console.WriteLine("\t n <message>: to add a new message");
-            Console.WriteLine("\t p <numberOfMessages>: to get first <numberOfMessages> messages");
+            Console.WriteLine("\t p <numberOfMessages>: to peak first <numberOfMessages> messages");
+            Console.WriteLine("\t r <numberOfMessages>: to read first <numberOfMessages> messages");
+        }
+
+        private static async Task<QueueClient> GetQueueClient()
+        {
+            var queueClient = new QueueClient(connectionStr, queueName);
+
+            await queueClient.CreateIfNotExistsAsync();
+
+            return queueClient;
         }
     }
 }
