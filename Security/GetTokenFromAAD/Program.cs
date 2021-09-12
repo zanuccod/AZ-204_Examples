1431cce5ad7d30fb1937e600dba886584d3ea692
Davide Zanucco <zanuccod@gmail.com>
2021-09-12 17:22:50 +0200

    Add KeyVault Examples

---
+++ Security/GetTokenFromAAD/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+
+namespace GetTokenFromAAD
+{
+    public class Program
+    {
+        // data defined on App Registration on AAD
+        private const string TENNANT_ID = "b00367e2-193a-4f48-94de-7245d45c0947";
+        private const string CLIENT_ID = "b5f53607-b6a2-42e6-b38c-e16c95ea1219";
+        private const string REDIRECT_URI = "http://localhost";
+
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Try to login...");
+
+            try
+            {
+                var appInstance = PublicClientApplicationBuilder.
+                    Create(CLIENT_ID)
+                    .WithAuthority(AzureCloudInstance.AzurePublic, TENNANT_ID)
+                    .WithRedirectUri(REDIRECT_URI)
+                    .Build();
+                string[] scopes = { "user.read" };
+
+                // Get Access Token from AAD
+                var result = await appInstance
+                    .AcquireTokenInteractive(scopes)
+                    .ExecuteAsync();
+
+                Console.WriteLine($"Received token:\n{result.AccessToken}");
+
+                // Get resource from Microsoft Graph
+
+                var provider = new InteractiveAuthenticationProvider(appInstance, scopes);
+                var client = new GraphServiceClient(provider);
+
+                var me = await client
+                    .Me
+                    .Request()
+                    .GetAsync();
+
+                Console.WriteLine($"Display Name:\t{me.DisplayName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unable to get the token: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Security/KeyVault/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Keys;
+using Azure.Security.KeyVault.Keys.Cryptography;
+using Azure.Security.KeyVault.Secrets;
+
+namespace KeyVault
+{
+    /*
+     * Prerequisites
+     *  - register an app to AAD (Azure Actvive Directory), generate a secret and
+     *    and retrieve ClientId, TenantId and the ClientSecret
+     *  - create the resource AzureKeyVault and add the AccessPolicy to the 
+     *    previous created app on AA
+     */
+    public class Program
+    {
+        private const string TENNANT_ID = "b00367e2-193a-4f48-94de-7245d45c0947";
+        private const string CLIENT_ID = "b5f53607-b6a2-42e6-b38c-e16c95ea1219";
+        private const string CLIENT_SECRET = "3VH~d740yvrU~K1xZxrwpl3l37E8~1b.ZY";
+
+        private const string KEY_VAULT_URL = "https://keyvaulttestricopegnata.vault.azure.net/";
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                await GetSecretFromKeyVault();
+                await EncryptValueWithKey();
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unable to complete the operation: {ex.Message}");
+            }
+        }
+
+        private static async Task GetSecretFromKeyVault()
+        {
+            const string SECRET_NAME = "KeyVaultTestSecret";
+            Console.WriteLine($"Trying to retrieve the value of the secret with name <{SECRET_NAME}>");
+
+            var clientCredentials = GetClientSecretCredential();
+            var secretClient = new SecretClient(new Uri(KEY_VAULT_URL), clientCredentials);
+
+            var secretValue = await secretClient
+                .GetSecretAsync(SECRET_NAME);
+
+            Console.WriteLine($"The retrieved value is : <{secretValue.Value.Value}>");
+        }
+
+        private static async Task EncryptValueWithKey()
+        {
+            const string KEY_NAME = "KeyVaultEncryptKey";
+            const string phraseToEncrypt = "pippoPlutoToEncrypt";
+
+            var clientCredentials = GetClientSecretCredential();
+            var keyClient = new KeyClient(new Uri(KEY_VAULT_URL), clientCredentials);
+
+            var encryptionKey = await keyClient
+                .GetKeyAsync(KEY_NAME);
+
+            var encryptionClient = new CryptographyClient(encryptionKey.Value.Id, clientCredentials);
+
+
+            var phraseToEncryptInBytes = Encoding.UTF8.GetBytes(phraseToEncrypt);
+            var encryptedPhrase = await encryptionClient
+                .EncryptAsync(EncryptionAlgorithm.RsaOaep, phraseToEncryptInBytes);
+
+            Console.WriteLine($"The encrypted text:\n {Convert.ToBase64String(encryptedPhrase.Ciphertext)}");
+
+            var decryptedText = await encryptionClient
+                .DecryptAsync(EncryptionAlgorithm.RsaOaep, encryptedPhrase.Ciphertext);
+            Console.WriteLine($"The deencrypted text:\n {Encoding.UTF8.GetString(decryptedText.Plaintext)}");
+        }
+
+        private static ClientSecretCredential GetClientSecretCredential()
+        {
+            return new ClientSecretCredential(TENNANT_ID, CLIENT_ID, CLIENT_SECRET);
+        }
+    }
+}
