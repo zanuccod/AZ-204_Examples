475706cb76f80bd7335cf95a0c0666501ca45465
Davide Zanucco <zanuccod@gmail.com>
2021-09-18 10:47:11 +0200

    Update Program.cs

--- Security/GetTokenFromAAD/Program.cs
+++ Security/GetTokenFromAAD/Program.cs
@@ -8,43 +8,12 @@ namespace GetTokenFromAAD
 {
     public class Program
     {
-        // data defined on App Registration on AAD
-        private const string TENNANT_ID = "";
-        private const string CLIENT_ID = "";
-        private const string REDIRECT_URI = "http://localhost";
-
         public static void Main(string[] args)
         {
             Console.WriteLine("Try to login...");
 
             try
             {
-                //var appInstance = PublicClientApplicationBuilder.
-                //    Create(CLIENT_ID)
-                //    .WithAuthority(AzureCloudInstance.AzurePublic, TENNANT_ID)
-                //    .WithRedirectUri(REDIRECT_URI)
-                //    .Build();
-                //string[] scopes = { "user.read" };
-
-                //// Get Access Token from AAD
-                //var result = await appInstance
-                //    .AcquireTokenInteractive(scopes)
-                //    .ExecuteAsync();
-
-                //Console.WriteLine($"Received token:\n{result.AccessToken}");
-
-                //// Get resource from Microsoft Graph
-
-                //var provider = new InteractiveAuthenticationProvider(appInstance, scopes);
-                //var client = new GraphServiceClient(provider);
-
-                //var me = await client
-                //    .Me
-                //    .Request()
-                //    .GetAsync();
-
-                //Console.WriteLine($"Display Name:\t{me.DisplayName}");
-
                 var publicApp = BuildPublicClientApplication();
                 var confidentialApp = BuildConfidentialClientApplication();
 
